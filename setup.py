fd5db31160795d326fb15100cb3a1f7224546ffe
Whit Morriss <whit@surveymonkey.com>
2010-10-07 23:50:07 +0800

    add flask and pip as extensions

--- setup.py
+++ setup.py
@@ -5,21 +5,18 @@ version = '0.1'
 
 setup(name='cheese_emporium',
       version=version,
-      description="PyPi clone",
-      long_description="""\
-""",
+      description="static PyPi clone",
+      long_description=""" """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='Distribution, pip',
       author='Matt George',
       author_email='matt.george@gmail.com',
       url='',
       license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=find_packages(exclude=[]),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
+      install_requires=["flask", "pip"],
       entry_points="""
       # -*- Entry points: -*-
       """,
